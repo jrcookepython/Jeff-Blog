cac284caf72c8e07df689137b7702b33d88aeefe
Jeff Cooke <jrcookepython@gmail.com>
2023-06-29 14:00:29 -0400

    Add videos

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, FileField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
@@ -9,6 +9,7 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
+    video = StringField("Video Post")
     submit = SubmitField("Submit Post")
 
 class CommentForm(FlaskForm):
