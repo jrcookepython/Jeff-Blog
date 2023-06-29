81c2efd79c5947fbc49019c931a9153c99e49d42
Jeff Cooke <jrcookepython@gmail.com>
2023-06-29 15:33:55 -0400

    test drop tables

--- delete_table.py
+++ delete_table.py
@@ -1,21 +1,3 @@
-import sqlite3
+from main import db
 
-conn = sqlite3.connect('blog.dv')
-
-cursor = conn.cursor()
-TABLE1 = "blog_posts"
-TABLE2 = "User"
-TABLE3 = "comments"
-
-cursor.execute(f"DROP TABLE {TABLE1}")
-print(f"Dropped Table {TABLE1}")
-
-cursor.execute(f"DROP TABLE {TABLE2}")
-print(f"Dropped Table {TABLE2}")
-
-cursor.execute(f"DROP TABLE {TABLE3}")
-print(f"Dropped Table {TABLE3}")
-
-conn.commit()
-
-conn.close()
+db.drop_all()
