6f2e01734fdcedc0043b62797c96efe96329d3c9
Jeff Cooke <jrcookepython@gmail.com>
2023-06-29 14:41:46 -0400

    Add videos 2

--- forms.py
+++ forms.py
@@ -8,8 +8,8 @@ class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
     video = StringField("Video Post")
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 class CommentForm(FlaskForm):
